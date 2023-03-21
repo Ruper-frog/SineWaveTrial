15e0628864bda56a01beb879ee8a1d59cd8cb74c
Ruper Ottolenghi <ruper120406@gmail.com>
2023-03-21 22:34:39 +0200

    NoteDone

--- SineWaveTrial/Program.cs
+++ SineWaveTrial/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Threading;
 
 namespace SineWaveTrial
 {
@@ -22,7 +21,7 @@ namespace SineWaveTrial
         {
             string[,] DRAWING_GAME = new string[11, 12];
 
-            int[,] Lengths = new int[12, 12];
+            int[] Lengths = new int[12];
 
             string prompt = @"                                                                                                   
 ▓█████▄  ██▀███   ▄▄▄       █     █░ ██▓ ███▄    █   ▄████      ▄████  ▄▄▄       ███▄ ▄███▓▓█████
@@ -205,34 +204,28 @@ namespace SineWaveTrial
 
             SetToZero(Lengths);
 
-            void SetToZero(int[,] Length)
+            void SetToZero(int[] Length)
             {
                 for (int i = 0; i < Lengths.GetLength(0); i++)
                 {
-                    for (int j = 0; j < Length.GetLength(1); j++)
-                    {
-                        Lengths[i, j] = 0;
-                    }
+                    Lengths[i] = 0;
                 }
             }
 
             for (int i = 0; i < DRAWING_GAME.GetLength(0); i++)
             {
-                for (int j = 0; j < DRAWING_GAME.GetLength(1); j++)
-                {
-                    Lengths[i + 1, j] = DRAWING_GAME[i, j].Length;
-                }
+                Lengths[i + 1] = DRAWING_GAME[i, 0].Length;
             }
 
-            Sum(Lengths);
+            PrintArray(Lengths);
 
-            //PrintArray(Lengths);
+            Sum(Lengths);
 
             Console.Write("\n");
 
             Console.WriteLine("\n\n");
 
-            //PrintArray(Lengths);
+            PrintArray(Lengths);
 
             Console.ForegroundColor = ConsoleColor.DarkRed;
 
@@ -240,33 +233,26 @@ namespace SineWaveTrial
 
             //Normal(prompt);
 
-            LetMeHaveAGo(DRAWING_GAME, Lengths);
         }
-        static void PrintArray(int[,] Lengths)
+        static void PrintArray(int[] Lengths)
         {
-            for (int i = 0; i < Lengths.GetLength(0); i++)
+            for (int i = 0; i < Lengths.Length; i++)
             {
-                for (int j = 0; j < Lengths.GetLength(1); j++)
-                {
-                    Console.Write(Lengths[i, j] + "\t|");
-                }
+                Console.Write(Lengths[i] + "\t|");
                 Console.WriteLine();
             }
         }
-        static void Sum(int[,] Lengths)
+        static void Sum(int[] Lengths)
         {
-            for (int i = 0; i < Lengths.GetLength(1); i++)
+            for (int i = 0; i < Lengths.Length; i++)
             {
-                for (int j = Lengths.GetLength(0) - 1; j > 0; j--)
-                {
-                    int Count = 0;
+                int Count = 0;
 
-                    for (int T = j; T > 0; T--)
-                    {
-                        Count += Lengths[T, i];
-                    }
-                    Lengths[j, i] = Count;
+                for (int T = Lengths.Length - 1; T > 0; T--)
+                {
+                    Count += Lengths[T];
                 }
+                Lengths[i] = Count;
             }
         }
         static void PrintLetter(string[,] DRAWING_GAME, int Row, int[,] Lengths, int Sine)
@@ -289,7 +275,7 @@ namespace SineWaveTrial
                     int Sine = (int)Math.Round((Math.Sin(seconds + 0.6 * i * -1) + 1) * 3);
 
                     PrintLetter(DRAWING_GAME, i, Lengths, Sine);
-                } 
+                }
             }
             Console.WriteLine();
         }
